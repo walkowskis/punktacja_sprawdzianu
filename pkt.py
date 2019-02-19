d61734086bd6e19065098f3cd3c6237430e7789e
walkowskis <46692044+walkowskis@users.noreply.github.com>
2019-02-19 20:47:07 +0100

    Update pkt.py

--- pkt.py
+++ pkt.py
@@ -13,27 +13,44 @@ def punktacja():
         cel=point*0.98
         bdb=point*0.91
         db=point*0.76
+        dst=point*0.51
+        dop=point*0.31
         Entry.insert(E2, 0, to_str(cel)+' - '+to_str(point))
         Entry.insert(E3, 0, to_str(bdb)+' - '+to_str(cel-0.01))
         Entry.insert(E4, 0, to_str(db)+' - '+to_str(bdb-0.01))
-        print(Na5)
+        Entry.insert(E5, 0, to_str(dst)+' - '+to_str(db-0.01))
+        Entry.insert(E6, 0, to_str(dop)+' - '+to_str(dst-0.01))
+        Entry.insert(E7, 0, to_str(0)+' - '+to_str(dop-0.01))
+
     except ValueError:
-        messagebox.showinfo("Uwaga!","Please enter the value in integer")
+        messagebox.showinfo("Uwaga!","Wprowadź poprawne wartości liczbowe!")
 def clear():
     E2.delete(0, 'end')
     E3.delete(0, 'end')
+
 top = Tk()
+#rozmiar i położeni okna
+win_width=500
+win_height=500
+screen_width=top.winfo_screenwidth()
+screen_height=top.winfo_screenheight()
+x_pos=(screen_width/2) - (win_width/2)
+y_pos=(screen_height/2) - (win_height/2)
+top.geometry('%dx%d+%d+%d' % (win_width, win_height, x_pos, y_pos))
+
 top.title("WSO")
+
 L1 = Label(top, text="Punktacja sprawdzianów",).grid(row=0,column=1)
-L2 = Label(top, text="Max punków:",).grid(row=1,column=0)
+L2 = Label(top, text="Max punków:",).grid(row=0,column=0)
 L3 = Label(top, text="Celujący",).grid(row=2,column=0)
 L4 = Label(top, text="Bardzo dobry",).grid(row=3,column=0)
 L4 = Label(top, text="Dobry",).grid(row=4,column=0)
 L5 = Label(top, text="Dostateczny",).grid(row=5,column=0)
 L6 = Label(top, text="Dopuszczający",).grid(row=6,column=0)
-L7 = Label(top, text="Niedostateczny",).grid(row=7,column=0)
+L7 = Label(top, text="Niedostateczny", fg='Red', font='Bold').grid(row=7,column=0)
+
 E1 = Entry(top, bd =5)
-E1.grid(row=1,column=1)
+E1.grid(row=1,column=3)
 E2 = Entry(top, bd =1)
 E2.grid(row=2,column=1)
 E3 = Entry(top, bd =1)
