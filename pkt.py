58488786ec2b63f6d2d0568c2e109139ce04a52f
walkowskis <46692044+walkowskis@users.noreply.github.com>
2019-02-19 22:45:30 +0100

    Update pkt.py
    Wersja z wynikami jako Label.

--- pkt.py
+++ pkt.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     from tkinter import *
 from tkinter import messagebox
+
 def punktacja():
     def to_str(x):
         y=str(round(x, 2))
@@ -15,18 +16,26 @@ def punktacja():
         db=point*0.76
         dst=point*0.51
         dop=point*0.31
-        Entry.insert(E2, 0, to_str(cel)+' - '+to_str(point))
-        Entry.insert(E3, 0, to_str(bdb)+' - '+to_str(cel-0.01))
-        Entry.insert(E4, 0, to_str(db)+' - '+to_str(bdb-0.01))
-        Entry.insert(E5, 0, to_str(dst)+' - '+to_str(db-0.01))
-        Entry.insert(E6, 0, to_str(dop)+' - '+to_str(dst-0.01))
-        Entry.insert(E7, 0, to_str(0)+' - '+to_str(dop-0.01))
-
+        var1 = StringVar()
+        l_cel = Label(top, textvariable=var1).grid(row=2, column=1)
+        var1.set(to_str(cel) + ' - ' + to_str(point))
+        var2 = StringVar()
+        l_bdb = Label(top, textvariable=var2).grid(row=3, column=1)
+        var2.set(to_str(bdb)+' - '+to_str(cel-0.01))
+        var3 = StringVar()
+        l_db = Label(top, textvariable=var3).grid(row=4, column=1)
+        var3.set(to_str(db)+' - '+to_str(bdb-0.01))
+        var4 = StringVar()
+        l_dst = Label(top, textvariable=var4).grid(row=5, column=1)
+        var4.set(to_str(dst)+' - '+to_str(db-0.01))
+        var5 = StringVar()
+        l_dop = Label(top, textvariable=var5).grid(row=6, column=1)
+        var5.set(to_str(dop)+' - '+to_str(dst-0.01))
+        var6 = StringVar()
+        l_ndst = Label(top, textvariable=var6).grid(row=7, column=1)
+        var6.set(to_str(0)+' - '+to_str(dop-0.01))
     except ValueError:
         messagebox.showinfo("Uwaga!","Wprowadź poprawne wartości liczbowe!")
-def clear():
-    E2.delete(0, 'end')
-    E3.delete(0, 'end')
 
 top = Tk()
 #rozmiar i położeni okna
@@ -49,20 +58,11 @@ L5 = Label(top, text="Dostateczny",).grid(row=5,column=0)
 L6 = Label(top, text="Dopuszczający",).grid(row=6,column=0)
 L7 = Label(top, text="Niedostateczny", fg='Red', font='Bold').grid(row=7,column=0)
 
+
 E1 = Entry(top, bd =5)
 E1.grid(row=1,column=3)
-E2 = Entry(top, bd =1)
-E2.grid(row=2,column=1)
-E3 = Entry(top, bd =1)
-E3.grid(row=3,column=1)
-E4 = Entry(top, bd =1)
-E4.grid(row=4,column=1)
-E5 = Entry(top, bd =1)
-E5.grid(row=5,column=1)
-E6 = Entry(top, bd =1)
-E6.grid(row=6,column=1)
-E7 = Entry(top, bd =1)
-E7.grid(row=7,column=1)
-B1=Button(top, text ="Punktacja",command = punktacja).grid(row=8,column=1,)
-B2=Button(top, text ="Punktacja opinia",command = clear).grid(row=9,column=1,)
+E1.focus()
+
+B1=Button(top, text ="Punktacja",command = punktacja).grid(row=8,columnspan=2,)
+B2=Button(top, text ="Punktacja opinia",command = punktacja).grid(row=9,column=1,)
 top.mainloop()
